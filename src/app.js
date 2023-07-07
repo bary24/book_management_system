c6d35c83d0b689f3ecf2fc01217f97ecdc91f81e
bary24 <ahmed_abdelbary@mivors.com>
2023-07-07 16:08:33 +0300

    Adding validate user middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const morgan = require("morgan");
+const jwt = require("jsonwebtoken");
 const routers = require("./routes/routers");
+const { ACCESS_TOKEN_SECRET } = require("../confg");
+console.log(ACCESS_TOKEN_SECRET);
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("combined"));
+app.use(async (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.sendStatus(403);
+    }
+
+    const accessToken = req.headers.authorization.split(" ")[1];
+
+    if (!accessToken) {
+        return res.sendStatus(403);
+    }
+
+    try {
+        const decoded = await jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
+        req.user = decoded.account;
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(403);
+    }
+
+    next();
+});
 
 app.use("/", routers);
 
